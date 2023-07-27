8a587efaa6f97a36c776f521ba7059cb39f8db73
Vittor Röpcke <vittorropcke@gmail.com>
2023-07-27 20:59:09 -0300

    Adicionando dependencia json e ajustando cenario de teste

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -1,6 +1,11 @@
 package br.ce.wcaquino.tasks.apitest;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.hamcrest.CoreMatchers;
+import org.json.JSONObject;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -26,8 +31,14 @@ public class APITest {
 	
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
+		String dataDeHoje = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("task", "Teste API");
+		requestParams.put("dueDate",dataDeHoje );
+		
+		
 		RestAssured.given()
-			.body("{\"task\": \"Teste API\",\"dueDate\": \"2023-07-24\"}")
+			.body(requestParams.toString())
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
